abb91f5bc6737792b38c684fdebe376a2ebdcb9e
Julian <julian.salamanca@est.iudigital.edu.co>
2026-04-08 12:53:45 -0500

    Imagen mas oscura para un mejor contraste con las letras

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import { getHomeInfo } from "@/services/get-home-info";
 
 export default async function Hero() {
   const strapiData = await getHomeInfo()
-  const {image, title, description} = strapiData  
+  const {image, title, description} = strapiData
 
   const style = {
     hero:"relative h-[80vh] flex flex-col items-center justify-center text-white bg-cover bg-center",
@@ -10,10 +10,12 @@ export default async function Hero() {
   
   return (
     <div id="hero" className={style.hero} style={{backgroundImage:`url(${image})`}}>
-      <div className="absolute inset-0 bg-black/40 brightness-75"> </div>
+      <div className="absolute inset-0 bg-black/80 z-1"> </div>
       
-      <div className="text-center px-4">
+      <div className="text-center px-4 z-10">
         <h1 className="text-5xl font-serif mb-4">{title}</h1>
+      </div>
+      <div className="text-center px-4 z-10">
         <p className="text-lg max-w-2xl font-light tracking-wide">{description}</p>
       </div>
     </div>
