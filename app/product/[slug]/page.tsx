53ff91082832a9eb0d3a903bd1d2421fb178d9ef
Julian <julian.salamanca@est.iudigital.edu.co>
2026-04-13 17:55:06 -0500

    La pagina de producto

---
+++ app/product/[slug]/page.tsx
@@ -0,0 +1,30 @@
+import { notFound } from "next/navigation";
+import getProduct from "@/services/get-product";
+import ProductDetail from "@/components/ui/ProductDetail";
+import { STRAPI_HOST } from "@/lib/config";
+
+interface PageProps {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function ProductPage({ params }: PageProps) {
+  const { slug } = await params;
+  const product = await getProduct(slug);
+
+  if (!product) {
+    notFound();
+  }
+
+  const productData = {
+    name: product.name,
+    description: product.description,
+    price: `$ ${product.price.toLocaleString()}`,
+    images: product.images.map((img: any) => (`${STRAPI_HOST}${img.url}`)),
+  };
+
+  return (
+    <main className="min-h-screen py-35">
+      <ProductDetail product={productData} />
+    </main>
+  );
+}
