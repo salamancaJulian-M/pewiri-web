fd892f282426e7265aec18e882da6f1db0f733d2
Julian <julian.salamanca@est.iudigital.edu.co>
2026-05-06 07:41:04 -0500

    Se borra informacion que no se va a usar
    Se borra fuente que ya no se va a usar y se cambia el favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import localFont from 'next/font/local';
 import { Merriweather } from 'next/font/google';
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
@@ -10,37 +9,6 @@ const merriweather = Merriweather({
   weight: ['300', '400', '700', '900'],
   style: 'normal',
   variable: '--font-merriweather'
-});
-
-const stolenLove = localFont({
-  src: [
-    {
-      path: './fonts/StolenLoveLight.otf',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: './fonts/StolenLove.otf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './fonts/StolenLoveBold.otf',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: './fonts/StolenLoveExtraBold.otf',
-      weight: '800',
-      style: 'normal',
-    },
-    {
-      path: './fonts/StolenLoveBlack.otf',
-      weight: '900',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-stolenLove',
 });
 
 export const metadata: Metadata = {
@@ -56,10 +24,10 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={`${stolenLove.variable} ${merriweather.variable} antialiased`}
+        className={`${merriweather.variable} antialiased flex flex-col min-h-screen`}
       >
         <Navbar />
-        <main>
+        <main className="flex-1">
           {children}
         </main>
         <Footer />
